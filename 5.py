8e7b04995bd0956aa27af6bb07f0c994ca692fca
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-19 23:36:37 +0100

    q5

---
+++ 5.py
@@ -0,0 +1,19 @@
+'''
+Convert Hours into Seconds
+
+Write a function that converts hours into seconds.
+
+Examples
+how_many_seconds(2) ➞ 7200
+
+how_many_seconds(10) ➞ 36000
+
+how_many_seconds(24) ➞ 86400
+Notes
+60 seconds in a minute, 60 minutes in an hour
+Don't forget to return your answer.
+
+'''
+
+def how_many_seconds(hours):
+    return hours * 3600
