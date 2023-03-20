2a3e07ec32ba895eef48869e62a21427bfaac924
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-20 19:45:30 +0100

    q9

---
+++ 9.py
@@ -0,0 +1,23 @@
+
+'''
+
+
+Return the First Element in a List
+
+
+Create a function that takes a list containing only numbers and return the first element.
+
+Examples
+get_first_value([1, 2, 3]) ➞ 1
+
+get_first_value([80, 5, 100]) ➞ 80
+
+get_first_value([-500, 0, 50]) ➞ -500
+Notes
+The first element in a list always has an index of 0.
+
+'''
+
+def get_first_value(number_list):
+    return number_list[8]
+
