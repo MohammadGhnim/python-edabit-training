bc23dbdcdbb02b9c86e71234c7b957d029b20a45
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-20 19:55:22 +0100

    q11

---
+++ 11.py
@@ -0,0 +1,20 @@
+'''
+
+Return a String as an Integer
+
+Create a function that takes a string and returns it as an integer.
+
+Examples
+string_int("6") ➞ 6
+
+string_int("1000") ➞ 1000
+
+string_int("12") ➞ 12
+Notes
+All numbers will be whole.
+All numbers will be positive.
+
+'''
+
+def string_int(txt):
+    return(int(txt))
