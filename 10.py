6e426ba5b09d91be589993063deafab2a4e7005d
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-20 19:50:40 +0100

    q10

---
+++ 10.py
@@ -0,0 +1,22 @@
+'''
+
+Area of a Triangle
+
+Write a function that takes the base and height of a triangle and return its area.
+
+Examples
+tri_area(3, 2) ➞ 3
+
+tri_area(7, 4) ➞ 14
+
+tri_area(10, 10) ➞ 50
+Notes
+The area of a triangle is: (base * height) / 2
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+'''
+
+def tri_area(base, height):
+    return (base * height) / 2
