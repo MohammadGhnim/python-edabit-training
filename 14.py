eaa1fafbbf211d6a90e388cd9651db23bd65b20c
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-20 20:06:54 +0100

    q14

---
+++ 14.py
@@ -0,0 +1,19 @@
+'''
+
+Boolean to String Conversion
+
+Create a function that takes a boolean variable flag and returns it as a string.
+
+Examples
+bool_to_string(True) ➞ "True"
+
+bool_to_string(False) ➞ "False"
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+'''
+
+def bool_to_string(flag):
+    return(str(flag))
