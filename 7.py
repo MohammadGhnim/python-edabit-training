560685423620079e8fd50ec90a4cf25bb74f92a5
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-20 19:31:49 +0100

    q7

---
+++ 7.py
@@ -0,0 +1,21 @@
+'''
+
+Convert Minutes into Seconds
+
+Write a function that takes an integer minutes and converts it to seconds.
+
+Examples
+convert(5) ➞ 300
+
+convert(3) ➞ 180
+
+convert(2) ➞ 120
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+'''
+
+def convert(minutes):
+    return minutes * 60
