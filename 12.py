28f1fc507ef91fcaa3140b5383082c3af986aec8
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-20 19:58:40 +0100

    q12

---
+++ 12.py
@@ -0,0 +1,22 @@
+'''
+
+To the Power of _____
+
+Create a function that takes a base number and an exponent number and returns the calculation.
+
+Examples
+calculate_exponent(5, 5) ➞ 3125
+
+calculate_exponent(10, 10) ➞ 10000000000
+
+calculate_exponent(3, 3) ➞ 27
+Notes
+All test inputs will be positive integers.
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+'''
+
+def calculate_exponent(num, exp):
+    return num ** exp
