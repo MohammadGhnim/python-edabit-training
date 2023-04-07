d3804b08a686b6549ba129042a7219c9072f8ba5
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-04-08 00:51:12 +0200

    18-

--- 18.py
+++ 18.py
@@ -19,7 +19,7 @@ def operation(num1, num2):
     elif num1 - num2 == 24 or num2 - num1 == 24:
         return 'subtracted'
     elif num1 / num2 == 24 or num2 / num1 == 24:
-        return 'dicided'
+        return 'divided'
     elif num1 * num2 == 24:
         return 'multiplied'
     else:
