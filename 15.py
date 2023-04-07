653f21e223e722f6f5ec2deb50f868aa9bf574bf
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-04-07 23:49:32 +0200

    15

---
+++ 15.py
@@ -0,0 +1,16 @@
+
+'''
+
+Are the Numbers Equal?
+
+create a function that takes two integers and checks if
+they are equal .
+
+Note: If there is a string then it should return False. 
+'''
+
+def is_equal(num1, num2):
+    if num1 == int(num2):
+        return True
+    else:
+        return False
