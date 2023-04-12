2837b4ced4b3fd269a1552fada92d8660b256ee5
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-04-12 17:19:36 +0200

    21

---
+++ 21.py
@@ -0,0 +1,24 @@
+
+
+'''
+Correct Inequality Signs:
+
+Create a function that returns True if a given inequality expression is correct and False otherwise.
+
+Examples:
+
+correct_signs("3 < 7 < 11") ➞ True
+
+correct_signs("13 > 44 > 33 > 1") ➞ False
+
+correct_signs("1 < 2 < 6 < 9 > 3") ➞ True
+
+
+Notes
+N/A
+
+'''
+
+def correct_signs(txt):
+    solution = eval(txt)
+    return solution
