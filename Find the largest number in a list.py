2fd1796afba7499481aed434d0fdf5676963b634
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-04-13 23:09:29 +0200

    23

---
+++ Find the largest number in a list.py
@@ -0,0 +1,15 @@
+
+
+'''
+
+Find the largest number in a list.
+
+create a function that takes a list of numbers.
+Return the largest number in the list.
+
+
+'''
+
+def findLargestNum(nums):
+    largest = max(nums)
+    return largest
