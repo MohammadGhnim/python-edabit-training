48561581039c0e54b333bfd229c004927c04e611
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-04-13 23:17:11 +0200

    24

---
+++ Return the first element in a list.py
@@ -0,0 +1,14 @@
+
+
+'''
+Return the first element in a list.
+
+create a function that takes a list containing only numbers
+and return the first element.
+
+
+'''
+
+def get_first_value(number_list):
+    value = number_list.pop(0)
+    return value
