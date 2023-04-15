c2f69559b730c405e897600fabff5615e5bc670b
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-04-15 23:57:25 +0200

    27

---
+++ Half, Quarter and Eighth.py
@@ -0,0 +1,22 @@
+
+'''
+Half, Quarter and Eighth:
+
+Create a function that takes a number and return a list of three numbers: half of the number,
+quarter of the number and an eighth of the number.
+
+Notes:
+
+- The order of the list is: half, quarter, eighth.
+
+'''
+
+def half_quarter_eight(n):
+    half = n/2
+    quarter = n/4
+    eight = n/8
+    numbers = []
+    numbers.append(half)
+    numbers.append(quarter)
+    numbers.append(eight)
+    return number
